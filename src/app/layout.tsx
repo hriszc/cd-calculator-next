df638175f73488c42ce3e7fbe73501b23254033c
hriszc <510245979@qq.com>
2025-12-07 11:55:55 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -22,6 +23,20 @@ export default function RootLayout({
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-02W0SWVKG9"
+        />
+        <Script id="ga-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-02W0SWVKG9');
+          `}
+        </Script>
+        {/* End Google tag (gtag.js) */}
       </head>
       <body className="antialiased">
         {/* Google Tag Manager (noscript) */}
